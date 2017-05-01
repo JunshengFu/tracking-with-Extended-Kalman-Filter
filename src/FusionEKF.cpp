371a8f6186ffed7a7417df55a387264647dfdba1
Junsheng Fu <junsheng.fu@yahoo.com>
2017-05-01 19:16:09 +0300

    style: Align the code in FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -45,9 +45,9 @@ FusionEKF::FusionEKF() {
 	// initialize the kalman filter variables
   ekf_.P_ = MatrixXd(4, 4);
   ekf_.P_ << 1, 0, 0, 0,
-	           0, 1, 0, 0,
-	           0, 0, 1000, 0,
-	           0, 0, 0, 1000;
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
 
   ekf_.F_ = MatrixXd(4, 4);
   ekf_.F_ << 1, 0, 1, 0,
