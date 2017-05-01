b0a5ebcbc50b606b032dbe952735565f7830203f
Junsheng Fu <junsheng.fu@yahoo.com>
2017-05-01 19:21:58 +0300

    style: Align the code in tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -16,37 +16,35 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     * Calculate the RMSE here.
   */
 
-	VectorXd rmse(4);
-	rmse << 0,0,0,0;
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
 
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	if(estimations.size() != ground_truth.size()
-			|| estimations.size() == 0){
-		std::cout << "Invalid estimation or ground_truth data" << std::endl;
-		return rmse;
-	}
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+    return rmse;
+  }
 
-	//accumulate squared residuals
-	for(unsigned int i=0; i < estimations.size(); ++i){
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
 
-		VectorXd residual = estimations[i] - ground_truth[i];
+    VectorXd residual = estimations[i] - ground_truth[i];
 
-		//coefficient-wise multiplication
-		residual = residual.array()*residual.array();
-		rmse += residual;
-	}
+    //coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
 
-	//calculate the mean
-	rmse = rmse/estimations.size();
+  //calculate the mean
+  rmse = rmse/estimations.size();
 
-	//calculate the squared root
-	rmse = rmse.array().sqrt();
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
 
-	//return the result
-	return rmse;
-
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -54,29 +52,29 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
-	MatrixXd Hj(3,4);
-	//recover state parameters
-	float px = x_state(0);
-	float py = x_state(1);
-	float vx = x_state(2);
-	float vy = x_state(3);
+  MatrixXd Hj(3,4);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
 
-	//pre-compute a set of terms to avoid repeated calculation
-	float c1 = px*px+py*py;
-	float c2 = sqrt(c1);
-	float c3 = (c1*c2);
+  //pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
 
-	//check division by zero
-	if(fabs(c1) < 0.0001){
-		std::cout << "Function CalculateJacobian() has Error: Division by Zero" << std::endl;
-		return Hj;
-	}
+  //check division by zero
+  if(fabs(c1) < 0.0001){
+    std::cout << "Function CalculateJacobian() has Error: Division by Zero" << std::endl;
+    return Hj;
+  }
 
-	//compute the Jacobian matrix
-	Hj << (px/c2), (py/c2), 0, 0,
-		      -(py/c1), (px/c1), 0, 0,
-		      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+  //compute the Jacobian matrix
+  Hj << (px/c2),                (py/c2),                0,      0,
+        -(py/c1),               (px/c1),                0,      0, 
+        py*(vx*py - vy*px)/c3,  px*(px*vy - py*vx)/c3,  px/c2,  py/c2;
 
-	return Hj;
+  return Hj;
 
 }
